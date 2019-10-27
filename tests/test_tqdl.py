cf9bb371b151383ab6839f10dbf75daecd7df4af
Shay Palachy <shaypal5@gmail.com>
2019-10-27 16:06:41 +0200

    test path should work on windows now

--- tests/test_tqdl.py
+++ tests/test_tqdl.py
@@ -1,7 +1,15 @@
 """Test tqdl."""
 
+import os
+
 from tqdl import download
 
 
+HOMEPATH = os.path.expanduser('~')
+
+
 def test_tqdl():
-    download("http://www.ovh.net/files/10Mb.dat", '/tmp/temp.dat')
+    download(
+        url="http://www.ovh.net/files/10Mb.dat",
+        fpath=os.path.join(HOMEPATH, 'tmp.dat'),
+    )
